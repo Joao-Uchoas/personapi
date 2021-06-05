44599946c47382bd7fb3760d6d449ec6936776d1
Joao.Uchoas <jvuchoas1998@gmail.com>
2021-06-05 13:19:05 -0300

    first commit

---
+++ src/main/java/com/estudos_dio/personapi/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.estudos_dio.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+    
+    @GetMapping
+    public String getBook(){
+        return "API Test";
+    }
+}
